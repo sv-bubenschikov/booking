6bcd95779f345d6f4cdbfcd4ad1d7c549debdcd6
MullayanovRustam <rustam.mullayanov@gmail.com>
2022-07-13 23:43:31 +0500

    Подключил PlaceAdapter к RecyclePlaces в PlacesFragment

--- app/src/main/java/com/example/bookingapp/app/fragments/places/PlacesFragment.kt
+++ app/src/main/java/com/example/bookingapp/app/fragments/places/PlacesFragment.kt
@@ -8,22 +8,32 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.bookingapp.R
 import com.example.bookingapp.databinding.FragmentCompaniesBinding
+import com.example.bookingapp.databinding.FragmentPlacesBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PlacesFragment : Fragment(R.layout.fragment_places) {
 
     private lateinit var viewModel: PlacesViewModel
+    private lateinit var placeAdapter: PlaceAdapter
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding = FragmentCompaniesBinding.bind(view)
+        val binding = FragmentPlacesBinding.bind(view)
 
-        binding.buttonNextFragment.setOnClickListener {
+        placeAdapter = PlaceAdapter {
             findNavController(view).navigate(R.id.action_placesFragment_to_bookingPlaceFragment)
+        }
+        with(binding) {
+            recyclePlaces.apply {
+                layoutManager = GridLayoutManager(this.context, 2)
+                adapter = placeAdapter
+            }
         }
     }
 }
