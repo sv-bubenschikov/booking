c9514c27048d8d9161d1c1ba98c8489ec958f9f3
MullayanovRustam <rustam.mullayanov@gmail.com>
2022-07-14 00:30:29 +0500

    Добавил верстку для chipFilter. В фрагменте добавил создание chip для проверки верстки

--- app/src/main/java/com/example/bookingapp/app/fragments/places/PlacesFragment.kt
+++ app/src/main/java/com/example/bookingapp/app/fragments/places/PlacesFragment.kt
@@ -6,18 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.bookingapp.R
 import com.example.bookingapp.databinding.FragmentCompaniesBinding
 import com.example.bookingapp.databinding.FragmentPlacesBinding
+import com.example.bookingapp.domain.entities.Place
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PlacesFragment : Fragment(R.layout.fragment_places) {
 
-    private lateinit var viewModel: PlacesViewModel
+    private val viewModel: PlacesViewModel by viewModels()
     private lateinit var placeAdapter: PlaceAdapter
 
 
@@ -34,6 +37,22 @@ class PlacesFragment : Fragment(R.layout.fragment_places) {
                 layoutManager = GridLayoutManager(this.context, 2)
                 adapter = placeAdapter
             }
+
+            // Пока что добавил заглушку, чтобы можно было проверить верстку
+            val attributes = listOf("1 этаж", "2 этаж", "3 этаж", "Переговорка", "4 этаж", "5 этаж")
+            for (attr in attributes) {
+                val chip =
+                    layoutInflater.inflate(R.layout.chip_filter_layout, chipsFilter, false) as Chip
+                chip.text = attr
+                chipsFilter.addView(chip)
+            }
         }
+
+        //Т.к id компании еще не пересылается из CompaniesFragment, поставил id-заглушку
+        showPlacesInfo(viewModel.getPlacesInfo(1))
+    }
+
+    private fun showPlacesInfo(places: List<Place>) {
+        placeAdapter.setPlaces(places)
     }
 }
--- app/src/main/java/com/example/bookingapp/app/fragments/places/PlacesViewModel.kt
+++ app/src/main/java/com/example/bookingapp/app/fragments/places/PlacesViewModel.kt
@@ -1,10 +1,17 @@
 package com.example.bookingapp.app.fragments.places
 
 import androidx.lifecycle.ViewModel
+import com.example.bookingapp.domain.entities.Place
+import com.example.bookingapp.domain.usecases.place.GetPlacesInfoByCompanyIdUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class PlacesViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+class PlacesViewModel @Inject constructor(
+    private val getPlacesInfoByCompanyIdUseCase: GetPlacesInfoByCompanyIdUseCase
+) : ViewModel() {
+
+    fun getPlacesInfo(companyId: Int): List<Place> {
+        return getPlacesInfoByCompanyIdUseCase(companyId)
+    }
 }
