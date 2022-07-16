5778586b00301a8202d729f117243428306f1461
Sergey Bubenshchikov <Sergey.V.Bubenshchikov@gmail.com>
2022-07-16 12:43:48 +0500

    Исправил проблему сборки и запуска приложения. Добавил vm для управления кнопкой действия, close #16

--- app/src/main/java/com/example/bookingapp/app/MainActivity.kt
+++ app/src/main/java/com/example/bookingapp/app/MainActivity.kt
@@ -1,19 +1,40 @@
 package com.example.bookingapp.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.bookingapp.R
-import com.example.bookingapp.app.fragments.booking.BookingFragment
+import com.example.bookingapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val hostViewModel by viewModels<HostViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            hostViewModel.onActionButtonClicked()
+        }
+        lifecycleScope.launch {
+            hostViewModel.actionButtonVisible.flowWithLifecycle(lifecycle).collect { isVisible ->
+                if (isVisible)
+                    binding.actionButton.show()
+                else
+                    binding.actionButton.hide()
+            }
+        }
 
         //issue #10 https://github.com/sv-bubenschikov/booking/projects/1#card-84219457
         val navHostFragment =
--- app/src/main/java/com/example/bookingapp/app/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/bookingapp/app/fragments/profile/ProfileFragment.kt
@@ -1,26 +1,24 @@
 package com.example.bookingapp.app.fragments.profile
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.bookingapp.R
-import com.example.bookingapp.databinding.FragmentCompaniesBinding
+import com.example.bookingapp.app.HostViewModel
 import com.example.bookingapp.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
+    private val hostViewModel: HostViewModel by activityViewModels()
     private lateinit var viewModel: ProfileViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentProfileBinding.bind(view)
 
+        hostViewModel.setActionButtonVisible(false)
     }
-
 }
