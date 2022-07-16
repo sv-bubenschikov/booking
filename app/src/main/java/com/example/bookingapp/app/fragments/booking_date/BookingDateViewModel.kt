4d82414fd52fb60d3955c3bc49ffe032ed84d253
Andrew Krysh <vip.krysh@mail.ru>
2022-07-16 19:05:18 +0500

    проверка добавления чипсов

--- app/src/main/java/com/example/bookingapp/app/fragments/booking_date/BookingDateViewModel.kt
+++ app/src/main/java/com/example/bookingapp/app/fragments/booking_date/BookingDateViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.bookingapp.app.fragments.booking_date
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class BookingDateViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class BookingDateViewModel @Inject constructor() : ViewModel() {
+    //fake data
 }
