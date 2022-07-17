595544b267e405956737f674beb06f852d47cb21
Andrew Krysh <vip.krysh@mail.ru>
2022-07-17 19:52:33 +0500

    Убрал лишние импорты

--- app/src/main/java/com/example/bookingapp/app/fragments/booking_date/BookingDateFragment.kt
+++ app/src/main/java/com/example/bookingapp/app/fragments/booking_date/BookingDateFragment.kt
@@ -1,7 +1,6 @@
 package com.example.bookingapp.app.fragments.booking_date
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
