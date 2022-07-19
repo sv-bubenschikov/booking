8f017d723055e6fab21081098425ff378c5f9c15
Andrew Krysh <vip.krysh@mail.ru>
2022-07-19 23:25:30 +0500

    Переписал на реактивное

--- app/src/main/java/com/example/bookingapp/domain/repositories_interface/DateRepository.kt
+++ app/src/main/java/com/example/bookingapp/domain/repositories_interface/DateRepository.kt
@@ -2,10 +2,11 @@ package com.example.bookingapp.domain.repositories_interface
 
 import com.example.bookingapp.domain.entities.Day
 import com.example.bookingapp.domain.entities.Period
+import kotlinx.coroutines.flow.StateFlow
 
 interface DateRepository {
 
-    fun getDaysInfoByPlaceId(placeId: Int): List<Day>
+    fun getDaysInfoByPlaceId(placeId: Int): StateFlow<List<Day>>
 
-    fun getPeriodsByDayId(dayId: Int): List<Period>
+    fun getPeriodsByDayId(dayId: Int): StateFlow<List<Period>>
 }
