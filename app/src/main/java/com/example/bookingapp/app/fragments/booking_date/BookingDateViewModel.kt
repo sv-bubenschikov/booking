923182923a443e55d697c07ba835f0c9024b00c3
Andrew Krysh <vip.krysh@mail.ru>
2022-07-20 00:26:26 +0500

    Изменение названия фрагмента на booking_date

---
+++ app/src/main/java/com/example/bookingapp/app/fragments/booking_date/BookingDateViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.bookingapp.app.fragments.booking_date
+
+import androidx.lifecycle.ViewModel
+
+class BookingDateViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
