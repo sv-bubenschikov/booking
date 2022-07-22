ae11fd9faa4c3f47c44235cd0a0f610778ffd38b
sv.bubenschikov <sv.bubenschikov@tensor.ru>
2022-07-22 20:40:26 +0500

    Пример подписки на изменение данных в RDB

--- app/src/main/java/com/example/bookingapp/app/fragments/companies/CompaniesViewModel.kt
+++ app/src/main/java/com/example/bookingapp/app/fragments/companies/CompaniesViewModel.kt
@@ -1,13 +1,18 @@
 package com.example.bookingapp.app.fragments.companies
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.bookingapp.domain.usecases.GetCompaniesInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class CompaniesViewModel @Inject constructor(
-    private val getCompaniesInfoUseCase: GetCompaniesInfoUseCase
+    getCompaniesInfoUseCase: GetCompaniesInfoUseCase
 ) : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val companies = getCompaniesInfoUseCase()
+        .stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 }
