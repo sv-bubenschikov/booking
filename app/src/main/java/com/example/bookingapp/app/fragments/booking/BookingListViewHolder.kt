126f22a679646427fd26dab53caaf3b18be42119
RoyalPiano <zelyoniyoleg@yandex.ru>
2022-07-27 02:08:37 +0500

    отображение booking + баги навигации

--- app/src/main/java/com/example/bookingapp/app/fragments/booking/BookingListViewHolder.kt
+++ app/src/main/java/com/example/bookingapp/app/fragments/booking/BookingListViewHolder.kt
@@ -3,6 +3,8 @@ package com.example.bookingapp.app.fragments.booking
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bookingapp.databinding.BookingItemBinding
 import com.example.bookingapp.domain.entities.Booking
+import kotlinx.coroutines.flow.map
+import org.joda.time.DateTime
 
 class BookingListViewHolder(
     private val binding: BookingItemBinding,
@@ -21,5 +23,14 @@ class BookingListViewHolder(
         data = item
         // TODO: #50
         companyTitle.text = data.companyName
+        placeName.text = data.placeName
+        theme.text = data.theme
+
+        dateTime.text = data.let {
+            val dateTime = DateTime(it.bookingDate).toLocalDateTime().toString("dd-MM-yyyy")
+            val startTime = DateTime(it.startTime).toLocalDateTime().toString("HH:mm")
+            val endTime = DateTime(it.endTime).toLocalDateTime().toString("HH:mm")
+            "$dateTime; $startTime - $endTime"
+        }
     }
 }
